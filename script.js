d646f3ad1eb2b58c5e20ea9021b6a6f21099f7f4
ja1antonio <ja1.negocios77@gmail.com>
2023-03-07 08:21:23 -0400

    Concerta Slide REVIEWS

--- script.js
+++ script.js
@@ -26,3 +26,24 @@ var swiper = new Swiper('.tool-slider', {
     },
   },
 });
+
+var swiper = new Swiper('.devs-slider', {
+  spaceBetween: 20,
+  grabCursor: true,
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  breakpoints: {
+    540: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
